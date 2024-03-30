6e62322fb31c74e1e11ce762961d4252c75de05c
Cody Chambers <grandcglitch@gmail.com>
2024-03-30 00:03:48 +0000

    added lfo

--- MySynth.cpp
+++ MySynth.cpp
@@ -8,11 +8,11 @@ using namespace daisy;
 DaisySeed hardware;
 
 static DaisySeed  hw;
-static Oscillator osc1, osc2;
+static Oscillator osc1, osc2, lfo;
 static MoogLadder flt;
 static CrossFade osccross;
 
-float  saw1, saw2 , freq, res;
+float  saw1, saw2 , freq, res, lfoOut;
 
 //12 bit max
 float potMax = 65535.0;
@@ -22,6 +22,8 @@ int detunePin1 = 18;
 int detunePin2 = 19;
 int filtfreqPin = 20;
 int filtResPin = 21;
+int lfoSpeedPin = 17;
+int lfoFiltPin = 16;
 
 
 static void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
@@ -31,8 +33,14 @@ static void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
     float sig, oscmix;
     for(size_t i = 0; i < size; i += 2)
     {
+
+        //set lfo speed and depth
+        lfo.SetFreq((10.0 * (hardware.adc.GetFloat(4)/potMax)));
+        lfo.SetAmp((hardware.adc.GetFloat(5)/potMax));
+        lfoOut = lfo.Process();
+
         //setup filter
-        freq = ((5000.0 * (hardware.adc.GetFloat(2) / potMax)) + 5000.0);
+        freq = (((5000.0 * (hardware.adc.GetFloat(2) / potMax)) + 5000.0) + (lfoOut*5000.0));
         res = (hardware.adc.GetFloat(3) / potMax);
         flt.SetFreq(freq);
         flt.SetRes(res);
@@ -67,17 +75,20 @@ int main(void)
     sample_rate = hw.AudioSampleRate();
     osc1.Init(sample_rate);
     osc2.Init(sample_rate);
+    lfo.Init(sample_rate);
     flt.Init(sample_rate);
 
     flt.SetRes(0.7);
 
     //setup pot inputs
-    AdcChannelConfig adcConfig[4];
+    AdcChannelConfig adcConfig[6];
     adcConfig[0].InitSingle(hardware.GetPin(detunePin1));
     adcConfig[1].InitSingle(hardware.GetPin(detunePin2));
     adcConfig[2].InitSingle(hardware.GetPin(filtfreqPin));
     adcConfig[3].InitSingle(hardware.GetPin(filtResPin));
-    hardware.adc.Init(adcConfig, 4);
+    adcConfig[4].InitSingle(hardware.GetPin(lfoSpeedPin));
+    adcConfig[5].InitSingle(hardware.GetPin(lfoFiltPin));
+    hardware.adc.Init(adcConfig, 6);
     hardware.adc.Start();
 
     hardware.Configure();
@@ -92,6 +103,11 @@ int main(void)
     osc2.SetWaveform(osc2.WAVE_SAW);
     osc2.SetFreq((440.0 * (hardware.adc.GetFloat(1) / potMax)) + 440.0);
     osc2.SetAmp(0.5);
+
+    //setup lfo
+    lfo.SetWaveform(lfo.WAVE_TRI);
+    lfo.SetFreq(1.0);
+    lfo.SetAmp(0.0);
 
     // start callback
     hw.StartAudio(AudioCallback);
