035309a540e899ac31468f0efb93bc9587a6ff13
Cody Chambers <grandcglitch@gmail.com>
2024-04-04 12:44:17 +0100

    Update Protosynth.cpp
    added overdrive

--- Protosynth.cpp
+++ Protosynth.cpp
@@ -8,9 +8,6 @@ using namespace daisysp;
 using namespace daisy;
 DaisySeed hardware;
 
-//setup multiplexer
-
-
 static DaisySeed  hw;
 static Oscillator osc, osc2, lfo1;
 static CrossFade osccross;
@@ -22,6 +19,8 @@ static MidiUsbHandler midi;
 static EchoSmpl echo;
 static Phaser phas;
 static MapUI mapUI[2];
+static Overdrive overd;
+
 
 //pick input pins
 //Osc Sec
@@ -49,25 +48,25 @@ int lfoToFilt = 2;
 int echoDur = 3;
 int echoFeed = 4;
 int phasePin = 5;
+int overdKnob = 6;
 
 // osc waveform selector
 int osc1state = 1;
 int osc2state = 1;
-
 int sin1 =1;
 int saw1 =2;
 int squ1 =3;
 int tri1 =4;
-
 int sin2 =5;
 int saw2 =6;
 int squ2 =7;
 int tri2 =8;
-
 int subState = 1;
+
 static Led led1;
 static Led lfoLed;
 static Led sin1Led, sin2Led, saw1Led, saw2Led, squ1Led, squ2Led, tri1Led, tri2Led;
+
 bool Gate;
 
 float lastFreq, env_out, lfo1Out, filLow, filHigh, filtMix;
@@ -82,28 +81,31 @@ static void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
     for(size_t i = 0; i < size; i += 2)
     {
 
+        //lfo process
         lfo1.SetFreq(hardware.adc.GetMux(0, 0));
         lfo1.SetAmp(1.0);
         lfo1Out = (lfo1.Process());
         lfoLed.Set(lfo1Out);
         lfoLed.Update();
 
-        float PhasAmount = hardware.adc.GetMux(0, 5);
+        //osc proccess
         float detuneAmount = -50 + (hardware.adc.GetFloat(1) * 100);
         float detuneAmount2 = -50 + (hardware.adc.GetFloat(2) * 100);
         osc.SetFreq(lastFreq + detuneAmount + (lfo1Out * hardware.adc.GetMux(0,1)));
         osc2.SetFreq((lastFreq / subState) + detuneAmount2 + (lfo1Out * hardware.adc.GetMux(0,1)));
         sig1 = osc.Process();
         sig2 = osc2.Process();
-
         osccross.SetPos(0.5);
         oscmix = osccross.Process(sig1, sig2);
 
+        //adsr
         ad1.SetTime(ADENV_SEG_ATTACK, hardware.adc.GetFloat(9));
         ad1.SetTime(ADENV_SEG_DECAY, hardware.adc.GetFloat(10));
         env_out = ad1.Process();
-
-        
+        osc.SetAmp(env_out);
+        osc2.SetAmp(env_out);
+        led1.Set(env_out);
+        led1.Update();
 
         //filter high low
         filter1.SetFreq((hardware.adc.GetFloat(6)*5000) + ((env_out * 5000) * hardware.adc.GetFloat(11)) + ((lfo1Out * 5000)* hardware.adc.GetMux(0,2)));
@@ -114,13 +116,7 @@ static void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
         filterMixCross.SetPos(hardware.adc.GetFloat(8));
         filterMix = filterMixCross.Process(filLow, filHigh);
         
-
-        
-        osc.SetAmp(env_out);
-        osc2.SetAmp(env_out);
-        led1.Set(env_out);
-        led1.Update();
-
+        //echo code
 	    float knob1,knob2;
 	    knob1 =  hardware.adc.GetMux(0, 3) * 200.0;
    	    knob2 =  hardware.adc.GetMux(0, 4);
@@ -129,14 +125,20 @@ static void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
         float filter_out = filter1.Low();
 	    float finalout = echo.Process(filter_out);
     
+        //phaser
+        float PhasAmount = hardware.adc.GetMux(0, 5);
         phas.SetLfoDepth(PhasAmount);
         float phaseOut = phas.Process(finalout);
 
+        //overdrive
+        overd.SetDrive(hardware.adc.GetMux(0,6));
+        float drive_out = overd.Process(phaseOut);
+
         // left out
-        out[i] = phaseOut * 0.5;
+        out[i] = drive_out * 0.5;
 
         // right out
-        out[i + 1] = phaseOut * 0.5;
+        out[i + 1] = drive_out * 0.5;
     }
 }
 
@@ -158,6 +160,7 @@ int main(void)
     phas.Init(sample_rate);
     phas.SetLfoDepth(1.f);
     phas.SetFreq(2000.f);
+    overd.Init();
 
     //setup envelope
     ad1.SetTime(ADENV_SEG_ATTACK, 0.15);
@@ -179,7 +182,6 @@ int main(void)
     squ2Led.Init(hardware.GetPin(7), false);
     tri2Led.Init(hardware.GetPin(8), false);
 
-
     AdcChannelConfig adcConfig[12];
     adcConfig[0].InitMux(seed::A0, 8,  seed::D11, seed::D12, seed::D13);
     // oscPins
@@ -188,17 +190,14 @@ int main(void)
     adcConfig[3].InitSingle (hardware.GetPin(oscMixPin));
     adcConfig[4].InitSingle (hardware.GetPin(pwKnob1));
     adcConfig[5].InitSingle (hardware.GetPin(pwKnob2));
-
     //filter pins
     adcConfig[6].InitSingle (hardware.GetPin(filtcut));
     adcConfig[7].InitSingle (hardware.GetPin(filtres));
     adcConfig[8].InitSingle (hardware.GetPin(filterMix));
-
     //adsr pins
     adcConfig[9].InitSingle (hardware.GetPin(attPin));
     adcConfig[10].InitSingle (hardware.GetPin(decPin));
     adcConfig[11].InitSingle (hardware.GetPin(adToFilt));
-
     hardware.adc.Init(adcConfig, 12);
     hardware.adc.Start();
 
@@ -226,6 +225,7 @@ int main(void)
     Switch subState1;
     subState1.Init(hardware.GetPin(29), 1000);
 
+    //set init wave leds
     sin1Led.Set(1);
     sin2Led.Set(1);
     sin1Led.Update();
